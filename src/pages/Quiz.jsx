1b245fe6855181e57414fc56396bf2d084eed7af
phamgialinhlx <phamgialinhlx2@gmail.com>
2023-07-24 21:33:08 +0700

    Improve UX

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -9,7 +9,7 @@ const Quiz = () => {
   const [choices, setChoices] = useState([-1, -1, -1, -1, -1])
   const [checkAnswers, setCheckAnswers] = useState(false)
   const [correctAnswers, setCorrectAnswers] = useState([])
-  
+
   const calculateScore = () => {
     let score = 0
     for (let i = 0; i < 5; i++) {
@@ -54,115 +54,69 @@ const Quiz = () => {
 
   return (
     <Fragment>
-      <Box
-        sx={{
-          width: '100vw',
-          height: '100vh',
-          display: 'flex',
-          background: '#F5F7FB',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
+      {quizzes.length > 0 ? (
         <Box
           sx={{
-            width: '1000px',
-            marginLeft: '75px',
+            width: '100vw',
+            height: '100vh',
+            display: 'flex',
+            background: '#F5F7FB',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
           }}
         >
-          {quizzes.map((quiz, quizIndex) => (
-            <Fragment key={quizIndex}>
-              <Typography
-                sx={{
-                  color: '#293264',
-                  fontFamily: 'Karla',
-                  fontSize: '16px',
-                  fontStyle: 'normal',
-                  fontWeight: '700',
-                  lineHeight: 'normal',
-                }}
-                dangerouslySetInnerHTML={{ __html: quiz.question }}
-              />
-              <Box pt={'11.98px'} pb={'14.99px'}>
-                {!checkAnswers ? (
-                  <Answers
-                    key={quizIndex}
-                    answers={quiz.answers}
-                    quizIndex={quizIndex}
-                    choices={choices}
-                    setChoices={setChoices}
-                  />
-                ) : (
-                  <Solution
-                    key={quizIndex}
-                    correctAnswers={correctAnswers}
-                    answers={quiz.answers}
-                    quizIndex={quizIndex}
-                    choices={choices}
-                    setChoices={setChoices}
-                  />
-                )}
-              </Box>
-              <Divider
-                sx={{
-                  width: '399px',
-                  height: '0.794px',
-                  marginBottom: '16px',
-                }}
-              />
-            </Fragment>
-          ))}
-        </Box>
-        {!checkAnswers ? (
-          <Button
-            sx={{
-              width: '120px',
-              height: '35px',
-              variant: 'contained',
-              borderRadius: '12px',
-              backgroundColor: '#4D5B9E',
-              color: '#FFFFFF',
-              textTransform: 'none',
-              fontFamily: 'Inter',
-              fontSize: '10.24px',
-              fontStyle: 'normal',
-              fontWeight: '600',
-              lineHeight: 'normal',
-              alignSelf: 'center',
-            }}
-            onClick={() => {
-              setCheckAnswers(true)
-            }}
-          >
-            Check answers
-          </Button>
-        ) : (
           <Box
             sx={{
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'center',
-              alignItems: 'center',
+              width: '1000px',
+              marginLeft: '75px',
             }}
           >
-            <Typography
-              sx={{
-                color: '#293264',
-                textAlign: 'center',
-                fontFamily: 'Inter',
-                fontSize: '12.8px',
-                fontStyle: 'normal',
-                fontWeight: '700',
-                lineHeight: 'normal',
-                marginRight: '16px',
-              }}
-            >
-              You scored {calculateScore()}/5 correct answers
-            </Typography>
+            {quizzes.map((quiz, quizIndex) => (
+              <Fragment key={quizIndex}>
+                <Typography
+                  sx={{
+                    color: '#293264',
+                    fontFamily: 'Karla',
+                    fontSize: '16px',
+                    fontStyle: 'normal',
+                    fontWeight: '700',
+                    lineHeight: 'normal',
+                  }}
+                  dangerouslySetInnerHTML={{ __html: quiz.question }}
+                />
+                <Box pt={'11.98px'} pb={'14.99px'}>
+                  {!checkAnswers ? (
+                    <Answers
+                      key={quizIndex}
+                      answers={quiz.answers}
+                      quizIndex={quizIndex}
+                      choices={choices}
+                      setChoices={setChoices}
+                    />
+                  ) : (
+                    <Solution
+                      key={quizIndex}
+                      correctAnswers={correctAnswers}
+                      answers={quiz.answers}
+                      quizIndex={quizIndex}
+                      choices={choices}
+                      setChoices={setChoices}
+                    />
+                  )}
+                </Box>
+                <Divider
+                  sx={{
+                    width: '399px',
+                    height: '0.794px',
+                    marginBottom: '16px',
+                  }}
+                />
+              </Fragment>
+            ))}
+          </Box>
+          {!checkAnswers ? (
             <Button
-              component={Link}
-              to="/"
               sx={{
                 width: '120px',
                 height: '35px',
@@ -182,11 +136,79 @@ const Quiz = () => {
                 setCheckAnswers(true)
               }}
             >
-              Play again
+              Check answers
             </Button>
-          </Box>
-        )}
-      </Box>
+          ) : (
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <Typography
+                sx={{
+                  color: '#293264',
+                  textAlign: 'center',
+                  fontFamily: 'Inter',
+                  fontSize: '12.8px',
+                  fontStyle: 'normal',
+                  fontWeight: '700',
+                  lineHeight: 'normal',
+                  marginRight: '16px',
+                }}
+              >
+                You scored {calculateScore()}/5 correct answers
+              </Typography>
+              <Button
+                component={Link}
+                to="/"
+                sx={{
+                  width: '120px',
+                  height: '35px',
+                  variant: 'contained',
+                  borderRadius: '12px',
+                  backgroundColor: '#4D5B9E',
+                  color: '#FFFFFF',
+                  textTransform: 'none',
+                  fontFamily: 'Inter',
+                  fontSize: '10.24px',
+                  fontStyle: 'normal',
+                  fontWeight: '600',
+                  lineHeight: 'normal',
+                  alignSelf: 'center',
+                }}
+                onClick={() => {
+                  setCheckAnswers(true)
+                }}
+              >
+                Play again
+              </Button>
+            </Box>
+          )}
+        </Box>
+      ) : (
+        <Box
+          sx={{
+            width: '100vw',
+            height: '100vh',
+            display: 'flex',
+            background: '#F5F7FB',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            color: '#293264',
+            fontFamily: 'Karla',
+            fontSize: '16px',
+            fontStyle: 'normal',
+            fontWeight: '700',
+            lineHeight: 'normal',
+          }}
+        >
+          Loading ...
+        </Box>
+      )}
     </Fragment>
   )
 }
